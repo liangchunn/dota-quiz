f01e315453ff8b96fb114d8c81aa1fdfa75638d1
Liang Chun <liangchunn@me.com>
2019-03-17 15:51:25 +0100

    Updates

---
+++ precache-manifest.07bcf1b9bd3b84d02e5a99a6393dc07d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "54b0b391a4f9e3ee70b6",
+    "url": "/dota-quiz/static/js/runtime~main.85622311.js"
+  },
+  {
+    "revision": "20dd57d85cdb1d47602b",
+    "url": "/dota-quiz/static/js/main.06505c94.chunk.js"
+  },
+  {
+    "revision": "ebec963277c582e07004",
+    "url": "/dota-quiz/static/js/2.b5db31d9.chunk.js"
+  },
+  {
+    "revision": "20dd57d85cdb1d47602b",
+    "url": "/dota-quiz/static/css/main.24ef8c21.chunk.css"
+  },
+  {
+    "revision": "b3a223a3a261dff7f797aaa9800063ab",
+    "url": "/dota-quiz/index.html"
+  }
+];
