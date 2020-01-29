ecfd124b9c1a50b95536b17408cffc46e875bd90
Liang Chun Wong <liang-chun.wong@trivago.com>
2020-01-29 23:08:21 +0100

    Updates

---
+++ precache-manifest.feedad27725563d74e7f414a1db100dd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "36f97f3ddbc75ba96ab5d64dd339851e",
+    "url": "/dota-quiz/index.html"
+  },
+  {
+    "revision": "ea53de9675d9e226bf02",
+    "url": "/dota-quiz/static/css/main.64ed7512.chunk.css"
+  },
+  {
+    "revision": "b9a32a917413a5be5fd1",
+    "url": "/dota-quiz/static/js/2.e6007da9.chunk.js"
+  },
+  {
+    "revision": "34c4edce79ad34e2a862c734dffcfc8b",
+    "url": "/dota-quiz/static/js/2.e6007da9.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ea53de9675d9e226bf02",
+    "url": "/dota-quiz/static/js/main.47f82a31.chunk.js"
+  },
+  {
+    "revision": "dee9662c26484e7d8622",
+    "url": "/dota-quiz/static/js/runtime-main.5f301ca8.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -11,13 +11,19 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/dota-quiz/precache-manifest.24e42afa0425fcee6da2bc40dcf7a92f.js"
+  "/dota-quiz/precache-manifest.feedad27725563d74e7f414a1db100dd.js"
 );
 
-workbox.clientsClaim();
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
 
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
@@ -25,10 +31,9 @@ workbox.clientsClaim();
  * See https://goo.gl/S9QRab
  */
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
-workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/dota-quiz/index.html", {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/dota-quiz/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
